e01e4c761111e3f6e5f98aca1a8f14d3c27a19f2
Khanh1802 <Khanh1802@github.com>
2023-05-15 11:11:37 +0700

    Update MVC ProductImage

--- Fashion-store-web/Controllers/ProductImageController.cs
+++ Fashion-store-web/Controllers/ProductImageController.cs
@@ -1,4 +1,5 @@
-﻿using Fashion_store_web.Service;
+﻿using Fashion_store_web.Dtos.ProductImage;
+using Fashion_store_web.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fashion_store_web.Controllers
@@ -18,5 +19,84 @@ namespace Fashion_store_web.Controllers
             return View(productImages);
         }
 
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductImageDto item)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productImageService.CreateAsync(item);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Update()
+        {
+
+            if (Request.RouteValues.TryGetValue("Id", out var productImageId))
+            {
+                if (Guid.TryParse(Convert.ToString(productImageId), out Guid id))
+                {
+                    var productImage = await _productImageService.GetById(id);
+                    if (productImage.Id != Guid.Empty && productImage.Id != null)
+                    {
+                        return View(new UpdateProductImageDto
+                        {
+                            ProductId = productImage.ProductId,
+                            Id = productImage.Id,
+                            Name = productImage.Name,
+                            ShortName = productImage.ShortName,
+                            Description = productImage.Description,
+                            Slug = productImage.Slug,
+                        });
+                    }
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateProductImageDto item)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productImageService.UpdateAsync(item);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete()
+        {
+
+            if (Request.RouteValues.TryGetValue("Id", out var productImageId))
+            {
+                if (Guid.TryParse(Convert.ToString(productImageId), out Guid id))
+                {
+                    var productImage = await _productImageService.GetById(id);
+                    if (productImage.Id != Guid.Empty && productImage.Id != null)
+                    {
+                        return View(productImage);
+                    }
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(ProductImageDto item)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productImageService.DeleteAsync(item.Id.Value);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
